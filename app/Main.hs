66ddc8edc2914a0a7588dbb2864756fe8b51271b
StanMey <stan_1998@hotmail.com>
2019-11-10 17:39:11 +0100

    Updated the CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,5 @@
 module Main (
   main
-, infoVertexMain
-, shPathMain
 ) where
 
 import qualified Lib as L
@@ -16,31 +14,44 @@ main = do
   userChoice <- getLine
   let chosenGraph = newGraph
   case read userChoice :: Int of
-    1 -> putStrLn $ show $ L.verticesAmount chosenGraph   -- gives the amount of existing Vertices
-    2 -> putStrLn $ show $ L.getAllVertices chosenGraph   -- gives all the existing Vertices
-    3 -> infoVertexMain chosenGraph                       -- gives all the connected Edges from a certain Vertex
-    4 -> shPathMain chosenGraph                           -- calculates the shortest path
-    5 -> return ()                                        -- quits the program
+    1 -> putStrLn $ show $ L.verticesAmount chosenGraph     -- gives the amount of existing Vertices
+    2 -> putStrLn $ show $ L.getAllVertices chosenGraph     -- gives all the existing Vertices
+    3 -> infoVertexMain chosenGraph 3                       -- gives all the connected Edges from a certain Vertex
+    4 -> putStrLn $ show $ L.getEdgesAmount chosenGraph     -- gives the amount of existing Edges
+    5 -> infoVertexMain chosenGraph 5                       -- gives the total amount of the weights of all the connected Edges
+    6 -> infoVerticesMain chosenGraph 6                     -- Checks if two Vertices are connected
+    7 -> infoVerticesMain chosenGraph 7                     -- Checks if two Vertices share a common Vertex
+    8 -> infoVerticesMain chosenGraph 8                     -- calculates the shortest path
+    9 -> return ()                                          -- quits the program
 
--- |Gives back all the connected Edges of a certain Vertex
-infoVertexMain :: Graph -> IO ()
-infoVertexMain gr = do
+
+-- |Gives back info from a certain Vertex based on the users choice
+infoVertexMain :: Graph -> Int -> IO ()
+infoVertexMain gr choice = do
   putStrLn "Which vertex?"
   fState <- getLine
-  let firstState = read fState :: Int
-  mapM_ putStrLn $ L.adjacentVerticesToString firstState gr
-  main
+  let chState = read fState :: Int
+  if not $ L.vertexExist chState gr
+    then do
+      putStrLn "The State doens't exist in the Graph"
+      infoVertexMain gr choice
+    else do
+      case choice of
+        3 -> mapM_ putStrLn $ L.adjacentVerticesToString chState gr
+        5 -> let vInfo = show $ L.getWeightSum chState gr
+             in putStrLn $ "All the connected Edges together have a weight of: " ++ vInfo
+      main
 
--- |Calculates the shortest path between 2 Vertices
-shPathMain :: Graph -> IO ()
-shPathMain gr = do
+-- |Gives back som info about the relation of two Vertices based on the users choice
+infoVerticesMain :: Graph -> IO ()
+infoVerticesMain gr choice = do
   putStrLn "\nWhat's the initial State?"
   fState <- getLine
   let firstState = read fState :: Int
   if not $ L.vertexExist firstState gr
     then do
       putStrLn "The State doesn't exist in the Graph"
-      shPathMain gr
+      infoVerticesMain gr choice
     else do
       putStrLn "What's the end State?"
       secState <- getLine
@@ -48,9 +59,12 @@ shPathMain gr = do
       if not $ L.vertexExist secondState gr
         then do
           putStrLn "The State doesn't exist in the Graph"
-          shPathMain gr
+          infoVerticesMain gr choice
         else do
-          putStrLn "Calculating the shortest path........."
+          case choice of
+            6 -> putStrLn $ show $ L.twoVerticesConn firstState secondState gr
+            7 -> putStrLn $ show $ L.getCommonVertices firstState secondState gr
+            8 -> putStrLn "Calculating the shortest path........."
           main
 
 
@@ -60,11 +74,15 @@ tupleToString list = map (\(a,b) -> show a ++ " - " ++ b) list
 
 -- |All the current choices the user can choose from
 choices :: [(Int, String)]
-choices = [(1, "Get the number of vertices in the Graph")
-          ,(2, "Get all vertices")
-          ,(3, "Get information about a specific vertex")
-          ,(4, "Get the shortest path")
-          ,(5, "Quit")
+choices = [(1, "Get the number of Vertices in the Graph")
+          ,(2, "Get all Vertices")
+          ,(3, "Get information about a specific Vertex")
+          ,(4, "Get the amount of existing Edges")
+          ,(5, "Get the total sum of all weights from one Vertex")
+          ,(6, "Check if two Vertices are connected")
+          ,(7, "Check if two Vertices share common Vertex/Vertices")
+          ,(8, "Get the shortest path")
+          ,(9, "Quit")
           ]
 
 
@@ -76,4 +94,5 @@ newGraph = Map.fromList
     ,(2, [(0, 2), (3, 1), (4, 7)])
     ,(3, [(1, 3), (2, 1), (4, 2)])
     ,(4, [(2, 7), (3, 2)])
+    ,(5, [])
     ]
