c0341ca623655a03491079294633918fedbbe44f
StanMey <stan_1998@hotmail.com>
2019-11-10 17:38:47 +0100

    Added functions to retrieve some information about possible connections

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,7 @@ import Datatypes
 import qualified Data.Map as Map
 
 
--- Some helper functions for getting information from the Graph
-
+-- *Some helper functions for getting the basic information from the Graph
 -- |Checks for the existence of a certain Vertex
 vertexExist :: Int -> Graph -> Bool
 vertexExist state gr = Map.member state gr
@@ -23,7 +22,7 @@ getAllVertices :: Graph -> [Vertex]
 getAllVertices = Map.keys
 
 
--- Functions to get all the information about the Edges
+-- *Functions to get information about the Edges
 -- |Get the total amount of Edges in the undirectional Graph
 getEdgesAmount :: Graph -> Int
 getEdgesAmount gr = (sum $ [countEdges x gr | x <- (getAllVertices gr)]) `div` 2
@@ -39,10 +38,11 @@ countEdges v gr = case getAdjacentVertices v gr of
 getWeightSum :: Vertex -> Graph -> Int
 getWeightSum state gr = sum $ map (\(a,b) -> b) $ getAllEdges state gr
 
+-- |Gets all the Edges from a certain Vertex
 getAllEdges :: Vertex -> Graph -> [Edge]
 getAllEdges v gr = case getAdjacentVertices v gr of
-  Nothing -> [(0,0)]
-  Just [] -> [(0,0)]
+  Nothing -> []
+  Just [] -> []
   Just all@(x:xs) -> all
 
 -- |Returns for a certain Vertex its edges with other vertices and weights
@@ -56,6 +56,39 @@ adjacentVerticesToString state map =
 -- |Puts info about the relation between a vertex and its edges in a list of Strings
 edgeToString :: Vertex -> [Edge] -> [String]
 edgeToString state connections = map (\(a,b) -> "From Vertex " ++ show state ++ " to Vertex " ++ show a ++ " with weight: " ++ show b) connections
+
+
+-- *Functions to get information about connections in the Graph
+-- |Gets all the adjacent connected Edges and returns all the Vertices of those Edges
+getOnlyConnVertices :: Vertex -> Graph -> [Vertex]
+getOnlyConnVertices v gr = map (\(a,b) -> a) $ getAllEdges v gr
+
+-- |Checks if two Vertices are connected
+twoVerticesConn :: Vertex -> Vertex -> Graph -> Bool
+twoVerticesConn v1 v2 gr = let a = getOnlyConnVertices v1 gr
+                           in v2 `elem` a
+
+-- |Returns any common connected Vertices between two Vertices
+getCommonVertices :: Vertex -> Vertex -> Graph -> [Vertex]
+getCommonVertices v1 v2 gr = let firstList = getOnlyConnVertices v1 gr
+                                 secondList = getOnlyConnVertices v2 gr
+                              in compareTwoLists firstList secondList
+
+-- |Checks if two Vertices share a common connected Vertex
+haveCommonVertices :: Vertex -> Vertex -> Graph -> Bool
+haveCommonVertices v1 v2 gr = let vAmount = length $ getCommonVertices v1 v2 gr
+                              in if vAmount > 0
+                                  then True
+                                  else False
+
+-- |Compares two lists and returns the common Vertices
+compareTwoLists :: [Vertex] -> [Vertex] -> [Vertex]
+compareTwoLists [] [] = []
+compareTwoLists (x:xs) [] = []
+compareTwoLists [] (x:xs) = []
+compareTwoLists (x:xs) all@(z:zs) = if x `elem` all
+                                      then x : compareTwoLists xs all
+                                      else compareTwoLists xs all
 
 
 
